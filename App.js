67706463ca8c5868d82415167304519eb7bb8f0e
positiveko <edie.spark1er@gmail.com>
2021-11-02 14:26:59 +0900

    feat: screen 및 navigator 추가

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
-import React, { useEffect } from "react";
-import auth from "@react-native-firebase/auth";
+import React, { useEffect, useState } from 'react';
+import auth from '@react-native-firebase/auth';
+import { NavigationContainer } from '@react-navigation/native';
+import InNav from './navigators/InNav';
+import OutNav from './navigators/OutNav';
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
-    console.log(auth().currentUser);
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
   }, []);
-  return null;
+  
+  return (
+    <NavigationContainer>
+      {isLoggedIn ? <InNav /> : <OutNav />}
+    </NavigationContainer>
+  );
 }
---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Home = () => (
+  <Container>
+    <Text>Home</Text>
+  </Container>
+);
+
+export default Home;
+
+const Container = styled.View``;
+
+const Text = styled.Text``;
